81bdf489be84ef9d7f0b00d2f7b52489734d42cf
alvarobem <alvaroberrocal12@gmail.com>
2017-05-29 14:43:40 +0200

    GRASP

---
+++ Builder.java
@@ -0,0 +1,13 @@
+package edp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alvaro Berrocal Martin - URJC
+ */
+public interface Builder {
+    
+    public ArrayList<Integer> build (int pos, Solution solution);
+    
+}
